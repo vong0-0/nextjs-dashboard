3ff4a11a9eca0aa59800dda38d9c521592f592b3
vong <vongsouvan09@gmail.com>
2025-06-22 00:15:04 +0700

    update

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { lusitana } from "../font";
+import { lusitana } from "@/app/ui/fonts";
 import Search from "@/app/ui/search";
 import {
   CustomersTableType,
